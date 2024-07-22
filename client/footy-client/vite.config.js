dfbae92137d8b0b9419a09c82f714161e63e2750
Stuart Pecksen <stuart151087@gmail.com>
2024-07-22 16:34:16 +0100

    .

--- client/footy-client/vite.config.js
+++ client/footy-client/vite.config.js
@@ -4,17 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  },
-  css: {
-    preprocessorOptions:{ 
-      less: {
-        modifyVars: {
-          'primary-color': '#1DA57A',
-          'heading-color': '#f00',
-        },
-        javascriptEnabled: true,
-      },
-    },
+    outDir: 'dist',
   },
 });
