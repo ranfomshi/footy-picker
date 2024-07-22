d04e0a95f74f8666fef589b0e7ff3aebe5584143
Stuart Pecksen <stuart151087@gmail.com>
2024-07-22 10:23:07 +0100

    initial build

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// API route
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from the server!' });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
