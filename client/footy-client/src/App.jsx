0d3876f10b95e6ad995781b50efeea5a3d6aeba2
Stuart Pecksen <stuart151087@gmail.com>
2024-07-26 22:18:24 +0100

    .

--- client/footy-client/src/App.jsx
+++ client/footy-client/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
     const fetchPlayers = async () => {
       try {
-          const response = await axios.get(`http://localhost:5000/api/players`);
+          const response = await axios.get(`${API_BASE_URL}/api/players`);
           setPlayers(response.data);
       } catch (error) {
           console.error("Error fetching players", error);
