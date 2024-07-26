a1c3e5aefa8c205688e911cac3235450d963e849
Stuart Pecksen <stuart151087@gmail.com>
2024-07-26 23:46:34 +0100

    .

--- client/footy-client/src/components/PlayerStats.jsx
+++ client/footy-client/src/components/PlayerStats.jsx
@@ -29,52 +29,60 @@ const PlayerStats = () => {
             dataIndex: 'name',
             key: 'name',
             sorter: (a, b) => a.name.localeCompare(b.name),
+            width: 100, // Set fixed width for the column
         },
         {
             title: 'Ws',
             dataIndex: 'wins',
             key: 'wins',
             sorter: (a, b) => a.wins - b.wins,
-            defaultSortOrder: 'descend', // Set default sort order to descending for wins column
-            width:40
+            defaultSortOrder: 'descend',
+            width: 50, // Set fixed width for the column
         },
         {
             title: 'Ls',
             dataIndex: 'losses',
             key: 'losses',
             sorter: (a, b) => a.losses - b.losses,
-            width:40
+            width: 50, // Set fixed width for the column
         },
         {
             title: 'Ds',
             dataIndex: 'draws',
             key: 'draws',
             sorter: (a, b) => a.draws - b.draws,
-            width:40
+            width: 50, // Set fixed width for the column
         },
         {
             title: 'G+',
             dataIndex: 'goalsFor',
             key: 'goalsFor',
             sorter: (a, b) => a.goalsFor - b.goalsFor,
-            width:40
+            width: 50, // Set fixed width for the column
         },
         {
             title: 'G-',
             dataIndex: 'goalsAgainst',
             key: 'goalsAgainst',
             sorter: (a, b) => a.goalsAgainst - b.goalsAgainst,
-            width:40
+            width: 50, // Set fixed width for the column
         },
     ];
-    
+
     return (
         <div>
             <h2>Player Stats</h2>
             {loading ? (
                 <Spin size="large" />
             ) : (
-                <Table size='small' dataSource={players} pagination={false} columns={columns} rowKey="id" />
+                <Table
+                    size="small"
+                    dataSource={players}
+                    columns={columns}
+                    rowKey="id"
+                    pagination={false} // Disable pagination to show all results
+                    scroll={{ y: '70vh', x: '100%' }} // Enable vertical and horizontal scrolling with fixed header
+                />
             )}
         </div>
     );
