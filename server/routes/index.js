65600eb0279ee6f82850ee414687a756dea092b6
Stuart Pecksen <stuart151087@gmail.com>
2024-10-28 23:02:59 +0000

    debug

--- server/routes/index.js
+++ server/routes/index.js
@@ -123,7 +123,7 @@ router.get('/current-player', protect, async (req, res) => {
     const playerId = req.user.playerId;  // This is set in the `protect` middleware
 
     if (!playerId) {
-      return res.status(404).json({ error: 'Player not found' });
+      return res.status(404).json({ error: 'Player not found 1' });
     }
 
     // Fetch the player information using the internal `id`
@@ -132,7 +132,7 @@ router.get('/current-player', protect, async (req, res) => {
     });
 
     if (!player) {
-      return res.status(404).json({ error: 'Player not found' });
+      return res.status(404).json({ error: 'Player not found 2' });
     }
 
     // Return the player's information
