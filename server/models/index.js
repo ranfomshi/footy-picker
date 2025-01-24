31441461a086d5deba73f050b587e82ab27b7225
Stuart Pecksen <stuart151087@gmail.com>
2025-01-24 09:30:53 +0000

    is admin flag added

--- server/models/index.js
+++ server/models/index.js
@@ -69,6 +69,11 @@ const RoomMembership = sequelize.define('RoomMembership', {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: false
+    },
+    isAdmin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
     }
 });
 
